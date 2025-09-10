18d2c0ebe707019e89d77d6bc80398c491bd49e4
byeongsu364 <kbsyjysbs@gmail.com>
2025-09-10 21:23:16 +0900

    ddd

--- src/sections/Instargram.jsx
+++ src/sections/Instargram.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import instar from '../util/instar'
-
+import instar from "../util/instar"
+import "../styles/sections/instar.scss"
 const Instargram = () => {
     return (
-        <section className="Instargram">
-            <div className="inner instar-inner">
-                <div className="t-wrap">
-                    <h2 className="tit">Instagram</h2>
-                    <p className="handle">@tocobo_official</p>
-                </div>
-
-                <ul className="instar-lst">
-                    {instar.map(({ id, image, alt, link }) => (
-                        <li className="card" key={id}>
-                            <Link to={link} aria-label={alt}>
-                                <img src={image} alt={alt} loading="lazy" />
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+        <div className='inner instar-inner'>
+            <div className="t-wrap">
+                <h2 className="tit">
+                    Instargram
+                </h2>
+                <p className="txt-4">
+                    @tocobo_official
+                </p>
             </div>
-        </section>
+            <ul className="instar-lst">
+                {instar.map((i) => (
+
+                    <li key={i.id}>
+                        <a
+                            style={{ backgroundImage: `url(${i.image})` }}
+                            alt={i.alt}
+                            href={i.link}>
+                            {i.alt}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
