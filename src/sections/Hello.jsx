1718190843e217a513470002383b2c0b2a4659fd
byeongsu364 <kbsyjysbs@gmail.com>
2025-09-18 21:10:35 +0900

    ㄴㄴ

--- src/sections/Hello.jsx
+++ src/sections/Hello.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { helloData } from '../util/hello'
-import "../styles/sections/hello.scss"
+import "../styles/sections/Hello.scss"
 const Hello = () => {
   return (
     <div className='inner hello-inner'>
